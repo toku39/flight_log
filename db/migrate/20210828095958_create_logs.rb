6f87014d7a1793e0ae0b103bd5142e35ac3b7d4a
toku39 <86103018+toku39@users.noreply.github.com>
2021-08-31 15:53:06 +0900

    logs_model追加

--- db/migrate/20210828095958_create_logs.rb
+++ db/migrate/20210828095958_create_logs.rb
@@ -1,6 +1,15 @@
 class CreateLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :logs do |t|
+      t.dateitme :date
+      t.string :airline
+      t.string :flight_num
+      t.integer :class_id
+      t.string :seat_num
+      t.integer :aircraft_type_id
+      t.string :aircraft_reqistration
+      
+
 
       t.timestamps
     end
